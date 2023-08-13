a2696ca93544ec48c49024858a817f6b43156166
diwadeep <141811766+diwadeep@users.noreply.github.com>
2023-08-13 23:37:24 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MarkdownModule } from 'ngx-md';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './tutorial/header.component';
-import { FooterComponent } from './tutorial/footer.component';
+// import { HeaderComponent } from './tutorial/header.component';
+// import { FooterComponent } from './tutorial/footer.component';
 
-import { UserComponent } from './example/user.component';       // Add an import to the component pointing to the place where it was created
+// import { UserComponent } from './example/user.component';
+//      // Add an import to the component pointing to the place where it was created
 
 @NgModule({
-  imports:      [
+  imports: [
     BrowserModule,
     FormsModule,
     NgbModule.forRoot(),
-    MarkdownModule.forRoot()
-    ],
-  declarations: [
-    AppComponent,
-    HeaderComponent
-    ],
-  bootstrap: [
-    AppComponent
-    ]
+    MarkdownModule.forRoot(),
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
